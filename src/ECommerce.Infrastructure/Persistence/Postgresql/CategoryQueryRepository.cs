d3d5f87d51de5b64904dbfe8ea7bf07d59b491e5
fnig2rm632 <fnig2rm632@yandex.ru>
2026-07-22 21:09:59 +0300

    feat(infrastructure): implement reposipories

---
+++ src/ECommerce.Infrastructure/Persistence/Postgresql/CategoryQueryRepository.cs
@@ -0,0 +1,36 @@
+using Dapper;
+using ECommerce.Application.Interfaces;
+using ECommerce.Application.SQRS.Response.ChemicalElements;
+using ECommerce.Domain.Common;
+using Npgsql;
+
+namespace ECommerce.Infrastructure.Persistence.Postgresql;
+
+public class CategoryQueryRepository(NpgsqlConnection connection) : ICategoryQueryRepository
+{
+    public async Task<Result<List<CategoryResponse>>> GetAllAsync()
+    {
+        try
+        {
+            const string query = @"SELECT * 
+                                   FROM categories";
+
+            var categoryList = await connection.QueryAsync<CategoryResponse>
+                (query);
+            
+            return categoryList.ToList();
+        }
+        catch (NpgsqlException)
+        {
+            return Error.Database.ConnectionFailed;
+        }
+        catch (TimeoutException)
+        {
+            return Error.Database.TimeoutGateway;
+        }
+        catch (Exception)
+        {
+            return Error.Database.InternalServer;
+        }
+    }
+}
---
+++ src/ECommerce.Infrastructure/Persistence/Postgresql/ChemicalElementQueryRepository.cs
@@ -0,0 +1,95 @@
+using Dapper;
+using ECommerce.Application.Interfaces;
+using ECommerce.Application.SQRS.Response.ChemicalElements;
+using ECommerce.Domain.Common;
+using Npgsql;
+
+namespace ECommerce.Infrastructure.Persistence.Postgresql;
+
+public class ChemicalElementQueryRepository(NpgsqlConnection connection) : IChemicalElementQueryRepository
+{
+    public async Task<Result<List<ChemicalElementResponse>>> GetAll()
+    {
+        try
+        { 
+            const string? query = @"SELECT * 
+                                    FROM chemical_elements";
+            
+            var elements = await connection.QueryAsync<ChemicalElementResponse>
+                (query);
+            
+            return elements.ToList();
+        }
+        catch (NpgsqlException)
+        {
+            return Error.Database.ConnectionFailed;
+        }
+        catch (TimeoutException)
+        {
+            return Error.Database.TimeoutGateway;
+        }
+        catch (Exception)
+        {
+            return Error.Database.InternalServer;
+        }
+    }
+
+    public async Task<Result<ChemicalElementResponse>> GetById(int id)
+    {
+        try
+        { 
+            const string? query = @"SELECT * 
+                                    FROM chemical_elements
+                                    WHERE id = @Id";
+            
+            var element = await connection.QuerySingleOrDefaultAsync<ChemicalElementResponse>
+                (query, new { Id = id });
+
+            if (element == null)
+                return Error.ChemicalElement.NotFound;
+            
+            return element;
+        }
+        catch (NpgsqlException)
+        {
+            return Error.Database.ConnectionFailed;
+        }
+        catch (TimeoutException)
+        {
+            return Error.Database.TimeoutGateway;
+        }
+        catch (Exception)
+        {
+            return Error.Database.InternalServer;
+        }
+    }
+
+    public async Task<Result<List<ChemicalElementResponse>>> GetByCategoryIds(List<int> categoryIds)
+    {
+        try
+        { 
+            const string? query = @"SELECT distinct e.id, e.symbol, e.name, e.atomic_mass, 
+                                    e.price_amount, e.price_currency, e.is_radioactive, e.is_synthetic
+                                    FROM chemical_elements e
+                                    INNER join element_categories ec on e.id = ec.element_id
+                                    where ec.category_id = Any(@CategoryIds)";
+            
+            var elements = await connection.QueryAsync<ChemicalElementResponse>
+                (query, new { CategoryIds = categoryIds } );
+            
+            return elements.ToList();
+        }
+        catch (NpgsqlException)
+        {
+            return Error.Database.ConnectionFailed;
+        }
+        catch (TimeoutException)
+        {
+            return Error.Database.TimeoutGateway;
+        }
+        catch (Exception)
+        {
+            return Error.Database.InternalServer;
+        }
+    }
+}
